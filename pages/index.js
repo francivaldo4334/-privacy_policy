734b5f2799ff2145f8f724122e141616eb457acd
francivaldo4334 <frncvldreis@gmail.com>
2022-03-31 13:49:29 -0300

    private politicy change 1

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+export default function Home(){
+    return(
+        <div>
+            <h2>
+                nome do desenvolvedor: Francivaldo Costa
+            </h2>
+            <h1>
+                Politica de privacidade
+            </h1>
+            <p>
+                este projeto nao requer nenhum tipo de informação pessoal
+            </p>
+            <h5>
+                * participacao de terceiros:
+            </h5>
+            <a href="https://policies.google.com/privacy?hl=pt-BR">
+            Google AdMob
+            </a>
+        </div>
+    );
+}
