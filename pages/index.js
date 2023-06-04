c3afb2e77d4689a87d23bb1e562434607a31c212
francivaldo <francivaldodev@gmail.com>
2023-06-04 11:31:58 -0300

    Gastin pro

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,32 @@
 export default function Home(){
     return(
         <div>
-            <h2>
-                nome do desenvolvedor: Francivaldo Costa
-            </h2>
             <h1>
-                Politica de privacidade
+            Política de Privacidade
             </h1>
-            <p>
-                este projeto nao requer nenhum tipo de informação pessoal
-            </p>
-            <h5>
-                * participacao de terceiros:
-            </h5>
-            <a href="https://policies.google.com/privacy?hl=pt-BR">
-            Google AdMob
-            </a>
+            <h1>
+Esta Política de Privacidade descreve como são tratados os dados de gastos pelo aplicativo de gestão de gastos ("o App") e como são protegidas as informações dos usuários. O App não coleta dados pessoais dos usuários e tem como objetivo principal fornecer uma maneira segura e eficiente para que os usuários possam gerenciar seus próprios gastos.
+
+    Coleta de Informações
+    O App não solicita nem coleta informações pessoais dos usuários. Não há necessidade de criar uma conta ou fornecer informações como nome, endereço de e-mail ou dados de autenticação. O App foi projetado para operar localmente no dispositivo do usuário e armazenar apenas as informações fornecidas pelo próprio usuário.
+
+    Dados de Gastos
+    O App permite que os usuários registrem e armazenem informações de gastos, incluindo valor, descrição e categoria. Esses dados são coletados exclusivamente para o propósito de ajudar os usuários a gerenciar e rastrear seus próprios gastos. O App não faz uso desses dados para qualquer outra finalidade, como fins publicitários ou compartilhamento com terceiros.
+
+    Armazenamento e Segurança
+    O App armazena os dados de gastos localmente no dispositivo do usuário. As informações são criptografadas e protegidas por meio de medidas de segurança apropriadas para evitar acesso não autorizado. É importante lembrar que a segurança de dispositivos móveis pode depender das configurações e proteções do próprio dispositivo do usuário.
+
+    Compartilhamento de Informações
+    O App não compartilha as informações de gastos com terceiros. Os dados registrados pelo usuário são mantidos estritamente no dispositivo local e não são transmitidos nem acessíveis por outras pessoas ou serviços.
+
+    Atualizações da Política de Privacidade
+    Esta Política de Privacidade pode ser atualizada periodicamente para refletir eventuais mudanças nas práticas do App. As alterações serão publicadas nesta página e os usuários serão notificados sobre quaisquer mudanças significativas.
+
+    Contato
+    Se você tiver alguma dúvida ou preocupação sobre esta Política de Privacidade ou sobre o tratamento de dados pelo App, entre em contato conosco através dos meios fornecidos no aplicativo.
+
+Ao usar o App, você concorda com esta Política de Privacidade e com o processamento dos seus dados de gastos da maneira descrita. É importante ler e entender esta política antes de utilizar o App.
+            </h1>
         </div>
     );
 }
