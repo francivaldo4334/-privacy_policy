19b696a04157b3084be71361deda8b3567b67a3b
francivaldo <francivaldodev@gmail.com>
2023-06-04 11:40:24 -0300

    fix

--- pages/index.js
+++ pages/index.js
@@ -4,29 +4,48 @@ export default function Home(){
             <h1>
             Política de Privacidade
             </h1>
-            <h1>
+            <h5>  
 Esta Política de Privacidade descreve como são tratados os dados de gastos pelo aplicativo de gestão de gastos ("o App") e como são protegidas as informações dos usuários. O App não coleta dados pessoais dos usuários e tem como objetivo principal fornecer uma maneira segura e eficiente para que os usuários possam gerenciar seus próprios gastos.
-
+</h5>
+    <h5>
     Coleta de Informações
+    </h5>
+    <h5>
     O App não solicita nem coleta informações pessoais dos usuários. Não há necessidade de criar uma conta ou fornecer informações como nome, endereço de e-mail ou dados de autenticação. O App foi projetado para operar localmente no dispositivo do usuário e armazenar apenas as informações fornecidas pelo próprio usuário.
-
+    </h5>
+    <h5>
     Dados de Gastos
+    </h5><h5>
     O App permite que os usuários registrem e armazenem informações de gastos, incluindo valor, descrição e categoria. Esses dados são coletados exclusivamente para o propósito de ajudar os usuários a gerenciar e rastrear seus próprios gastos. O App não faz uso desses dados para qualquer outra finalidade, como fins publicitários ou compartilhamento com terceiros.
-
+    </h5><h5>
     Armazenamento e Segurança
+    </h5><h5>
     O App armazena os dados de gastos localmente no dispositivo do usuário. As informações são criptografadas e protegidas por meio de medidas de segurança apropriadas para evitar acesso não autorizado. É importante lembrar que a segurança de dispositivos móveis pode depender das configurações e proteções do próprio dispositivo do usuário.
-
+    </h5>
+        <h5>
     Compartilhamento de Informações
+    </h5>
+
+    <h5>
     O App não compartilha as informações de gastos com terceiros. Os dados registrados pelo usuário são mantidos estritamente no dispositivo local e não são transmitidos nem acessíveis por outras pessoas ou serviços.
-
+    </h5>
+        <h5>
     Atualizações da Política de Privacidade
+    </h5>
+
+    <h5>
     Esta Política de Privacidade pode ser atualizada periodicamente para refletir eventuais mudanças nas práticas do App. As alterações serão publicadas nesta página e os usuários serão notificados sobre quaisquer mudanças significativas.
-
+    </h5>
+        <h5>
     Contato
-    Se você tiver alguma dúvida ou preocupação sobre esta Política de Privacidade ou sobre o tratamento de dados pelo App, entre em contato conosco através dos meios fornecidos no aplicativo.
+    </h5>
 
+    <h5>
+    Se você tiver alguma dúvida ou preocupação sobre esta Política de Privacidade ou sobre o tratamento de dados pelo App, entre em contato conosco através dos meios fornecidos no aplicativo.
+    </h5>
+        <h5>
 Ao usar o App, você concorda com esta Política de Privacidade e com o processamento dos seus dados de gastos da maneira descrita. É importante ler e entender esta política antes de utilizar o App.
-            </h1>
+        </h5>
         </div>
     );
 }
